fc146313fb87fbc8308cac4d44b70706979277d2
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-21 00:39:41 +0500

    Made transmitting token into a code more securely

--- src/ru/botgaydev/bot/Start.java
+++ src/ru/botgaydev/bot/Start.java
@@ -4,9 +4,22 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
 public class Start {
-    public static void main (String[] args) throws TelegramApiException {
+    public static void main (String[] args) throws TelegramApiException, IOException {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new BotTg("GoodsSeller", "")); // TODO use properties
+            botsApi.registerBot(new BotTg("GoodsSeller", getToken()));
+    }
+    private static String getToken() throws IOException {
+        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
+        String appConfigPath = rootPath + "ru/botgaydev/bot/config.properties";
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(appConfigPath));
+        return prop.getProperty("token");
     }
 }
