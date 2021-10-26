ad9dbc476fd8317b52333f42b1a8b4f439282dbe
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-10-26 15:47:23 +0500

    Removed unused dependencies in BotTg

--- src/ru/botgaydev/bot/BotTg.java
+++ src/ru/botgaydev/bot/BotTg.java
@@ -4,9 +4,6 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import java.util.concurrent.CompletableFuture;
-
-import java.util.Map;
 
 public class BotTg extends TelegramLongPollingBot {
     private final String BOT_NAME;
