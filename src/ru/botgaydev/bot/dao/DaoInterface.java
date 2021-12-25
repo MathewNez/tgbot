5d9a178d44a310398ba77d508449d0ad641a554d
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-12-25 13:19:23 +0500

    Added DAO interface

---
+++ src/ru/botgaydev/bot/dao/DaoInterface.java
@@ -0,0 +1,18 @@
+package ru.botgaydev.bot.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface DaoInterface<T, Id extends Serializable>{
+    public void persist(T entity);
+
+    public void update(T entity);
+
+    public T findById(Id id);
+
+    public void delete(T entity);
+
+    public List<T> findAll();
+
+    public void deleteAll();
+}
