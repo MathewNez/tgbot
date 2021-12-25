ef35e82ef969d9c4fa7a543c4f1f820a84c0bc41
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-12-25 13:21:21 +0500

    Added DAO classes for each entity

--- src/ru/botgaydev/bot/dao/AccountDao.java
+++ src/ru/botgaydev/bot/dao/AccountDao.java
@@ -1,4 +1,81 @@
 package ru.botgaydev.bot.dao;
 
-public class AccountDao {
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import ru.botgaydev.bot.entity.Account;
+
+import java.util.List;
+
+import static ru.botgaydev.bot.persistence.HibernateUtil.getSessionFactory;
+
+public class AccountDao implements DaoInterface<Account, String> {
+    private Session currentSession;
+    private Transaction currentTransaction;
+
+    public AccountDao() {
+
+    }
+
+    public Session openCurrentSession() {
+        currentSession = getSessionFactory().openSession();
+        return currentSession;
+    }
+
+    public Session openCurrentSessionwithTransaction() {
+        currentSession = getSessionFactory().openSession();
+        currentTransaction = currentSession.beginTransaction();
+        return currentSession;
+    }
+
+    public void closeCurrentSession() {
+        currentSession.close();
+    }
+
+    public void closeCurrentSessionwithTransaction() {
+        currentTransaction.commit();
+        currentSession.close();
+    }
+    public Session getCurrentSession() {
+        return currentSession;
+    }
+
+    public void setCurrentSession(Session currentSession) {
+        this.currentSession = currentSession;
+    }
+
+    public Transaction getCurrentTransaction() {
+        return currentTransaction;
+    }
+
+    public void setCurrentTransaction(Transaction currentTransaction) {
+        this.currentTransaction = currentTransaction;
+    }
+
+    public void persist(Account entity) {
+        getCurrentSession().save(entity);
+    }
+
+    public void update(Account entity) {
+        getCurrentSession().update(entity);
+    }
+
+    public Account findById(String id) {
+        return (Account) getCurrentSession().get(Account.class, id);
+    }
+
+    public void delete(Account entity) {
+        getCurrentSession().delete(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Account> findAll() {
+        return (List<Account>) getCurrentSession().createQuery("from Account").list();
+    }
+
+    public void deleteAll() {
+        List<Account> entityList = findAll();
+        for (Account entity : entityList) {
+            delete(entity);
+        }
+    }
 }
